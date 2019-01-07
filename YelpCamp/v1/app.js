4cba5d4cd5461876afc3a4633d900d1bab156fc1
Dewan Ahmed <dewan.ahmed@ibm.com>
2019-01-07 18:10:15 -0500

    YelpCamp v1

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -1,21 +1,44 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-app.get("/", function(req,res){
-  res.render("landing");
+var campgrounds = [
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+];
+    
+app.get("/", function(req, res){
+    res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-  var campgrounds = [
-    {name: "Dhaka", image: "https://upload.wikimedia.org/wikipedia/commons/d/d2/Dhaka_%2832709975263%29.jpg"},
-    {name: "Rajshahi", image: "https://www.motorcyclevalley.com/images/locations/rajshahi.jpg"},
-    {name: "Borishal", image: "https://upload.wikimedia.org/wikipedia/commons/a/af/Guthia_mosque%2CBarisal.jpg"}
-  ]
+    res.render("campgrounds",{campgrounds:campgrounds});
+});
 
-  res.render("campgrounds", {campgrounds: campgrounds});
-})
-app.listen(3000, function(req, res){
-  console.log("YelpCamp server is running!");
+app.post("/campgrounds", function(req, res){
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    //redirect back to campgrounds page
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+   res.render("new.ejs"); 
+});
+
+app.listen(3000, function(){
+   console.log("The YelpCamp Server Has Started!");
 });
